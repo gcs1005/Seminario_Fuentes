2b032b29793e811a26dbb8d84a0a95a75cac3ee2
ajcanepa <ajcanepa@gmail.com>
2022-11-07 12:52:40 +0100

    mejor orden del código

--- Trabajo_DAG/Script.R
+++ Trabajo_DAG/Script.R
@@ -1,13 +1,18 @@
+# Carga de paquetes -------------------------------------------------------
 library(dplyr)
-x1<-Muerte_niños_semana %>%
+
+# Carga de datos ----------------------------------------------------------
+x1 <- 
+  Muerte_niños_semana %>%
   group_by(`Comunidades y Ciudades Autónomas`, Sexo) %>%
   select(Total) %>%
   summarise(
     suma_total = sum(Total)
   )
-  na.omit(x1)
+na.omit(x1)
   
 x1
-x2<-na.omit(x1)
+x2 <- na.omit(x1)
 x2
-x3<-filter(x2,Sexo=="Ambos sexos")
+x3 <- filter(x2, Sexo == "Ambos sexos")
+
