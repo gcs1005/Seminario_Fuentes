5f43b3c4155136807f940006bc5bffb8b05fae26
GabrielCollado <gcs1005@alu.ubu.es>
2022-11-14 11:46:02 +0100

    comentario de ayuda para hacer el filtro de deporte de mujeres por comunidad autonoma

--- Script.R
+++ Script.R
@@ -16,6 +16,6 @@ muertes_CCAA <- filter(x2, Sexo == "Ambos sexos")
 muertes_CCAA #Visualizamos el filtrado
 
 # Filtrado datos deportes: Mujeres por CCAA y su nivel de act.física-------------------------------------------
-
-
+#utilizar funcion str() para ver el nombre de las columnas, y solo queda 
+#utilizar la función rename(), y cambiar su nombre a nivel bajo,moderado y alto
 
