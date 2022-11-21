d44e120426cc6a9cb184e4740727709215faecfe
Velaskini <avg1016@alu.ubu.es>
2022-11-21 10:24:33 +0100

    Cambio nombres

--- Input/functions/Importacion_datos.R
+++ Input/functions/Importacion_datos.R
@@ -1,11 +1,11 @@
 #Importación de los datos
 # *
 library(readr)
-Muerte_niños_semana <- read_delim("Input/data/02022.csv", 
+Muerte_niños_semana <- read_delim("Input/data/Muerte_niños_semana.csv", 
                                   delim = ";", escape_double = FALSE, trim_ws = TRUE)
 View(Muerte_niños_semana)
 
 # *
 library(readxl)
-ENS_2017 <- read_excel("Input/data/ENSE17_MOD3_REL (1).xlsx")
+ENS_2017 <- read_excel("Input/data/Encuesta_nacional_2017.xlsx")
 View(ENS_2017)
