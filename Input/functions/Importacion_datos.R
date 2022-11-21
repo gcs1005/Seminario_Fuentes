e69d342275aa318df0be89a2c7fd05d642423bce
GabrielCollado <gcs1005@alu.ubu.es>
2022-11-21 10:23:13 +0100

    Cambios en importacion

--- Input/functions/Importacion_datos.R
+++ Input/functions/Importacion_datos.R
@@ -1,11 +1,12 @@
 #Importación de los datos
 # *
 library(readr)
-Muerte_niños_semana <- read_delim("Input/data/02022.csv", 
+Muerte_niños_semana <- read_delim("Input/data/Muerte_niños_semana.csv", 
                                   delim = ";", escape_double = FALSE, trim_ws = TRUE)
 View(Muerte_niños_semana)
 
 # *
 library(readxl)
-ENS_2017 <- read_excel("Trabajo_DAG/Input/data/ENSE17_MOD3_REL (1).xlsx")
-View(ENS_2017)
+Encuesta_nacional_2017 <- read_excel("Input/data/Encuesta_nacional_2017.xlsx", 
+                                     sheet = "Hoja1")
+View(Encuesta_nacional_2017)
