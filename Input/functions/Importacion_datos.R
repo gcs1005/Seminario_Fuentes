b830c8ea72d6d1558b6b9ce4f550d644c72b22cd
dlp1004 <dlp1004@alu.ubu.es>
2022-11-28 08:44:39 +0100

    Limpio un poco el código

--- Input/functions/Importacion_datos.R
+++ Input/functions/Importacion_datos.R
@@ -3,12 +3,7 @@
 library(readr)
 Muerte_niños_semana <- read_delim("Input/data/Muerte_niños_semana.csv", 
                                   delim = ";", escape_double = FALSE, trim_ws = TRUE)
-
-
 # *
 library(readxl)
 Encuesta_nacional_2017 <- read_excel("Input/data/Encuesta_nacional_2017.xlsx", 
                                      sheet = "Hoja1")
-
-
-
