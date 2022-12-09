7f3add58503d80b7f557d0559cd4b9a1357603fe
Velaskini <avg1016@alu.ubu.es>
2022-12-09 18:11:09 +0100

    miau

--- Input/functions/Importacion_datos.R
+++ Input/functions/Importacion_datos.R
@@ -3,7 +3,9 @@
 library(readr)
 Muerte_niños_semana <- read_delim("Input/data/Muerte_niños_semana.csv", 
                                   delim = ";", escape_double = FALSE, trim_ws = TRUE)
+View(Muerte_niños_semana)
 # *
 library(readxl)
 Encuesta_nacional_2017 <- read_excel("Input/data/Encuesta_nacional_2017.xlsx", 
                                      sheet = "Hoja1")
+View(Encuesta_nacional_2017)
