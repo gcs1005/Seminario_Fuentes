341e9511c46ce3d3ba7e0deb50abc04f63206568
Velaskini <avg1016@alu.ubu.es>
2022-12-09 19:28:28 +0100

    No se pone view en la importación

--- Input/functions/Importacion_datos.R
+++ Input/functions/Importacion_datos.R
@@ -3,12 +3,8 @@
 library(readr)
 Muerte_niños_semana <- read_delim("Input/data/Muerte_niños_semana.csv", 
                                   delim = ";", escape_double = FALSE, trim_ws = TRUE)
-View(Muerte_niños_semana)
 # *
 library(readxl)
 Encuesta_nacional_2017 <- read_excel("Input/data/Encuesta_nacional_2017.xlsx", 
                                      sheet = "Hoja1")
-<<<<<<< HEAD
-View(Encuesta_nacional_2017)
-=======
->>>>>>> af3e5aa98e59b440e9b28de4b5c6fa2e6870b56b
+
